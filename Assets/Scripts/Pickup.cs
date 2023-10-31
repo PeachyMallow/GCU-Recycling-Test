2cd9c18deda7f4c433708883babee90ec9bb5e6a
TitanLight <jamesalexander2000@gmail.com>
2023-10-31 16:19:05 +0000

    Bin Functionality created
    The player is able to deposit the trash to raise their score

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Pickup : MonoBehaviour
 {
     public Text trashScore;
+    public Text score;
+    private int recycledScore;
     private int numTrash;
 
     void Start()
     {
         numTrash = 0;
+        recycledScore = 0;
         trashScore.text = "Trash Collected : " + numTrash;
+        score.text = "Trash Recycled : " + recycledScore;
     }
 
     private void OnTriggerEnter(Collider Trash)
@@ -21,6 +26,24 @@ public class Pickup : MonoBehaviour
             numTrash++;
             Destroy(Trash.gameObject);
             trashScore.text = "Trash Collected : " + numTrash;
+        }
+    }
+
+    private void OnTriggerStay(Collider trashBin)
+    {
+        if (trashBin.tag == "Bin")
+        {
+            if(Input.GetKey(KeyCode.E) && numTrash > 0)
+            {
+                recycledScore = numTrash;
+                score.text = "Trash Recycled : " + recycledScore;
+                numTrash = 0;
+                trashScore.text = "Trash Collected : " + numTrash;
+            }
+            else if(Input.GetKey(KeyCode.E) && numTrash <= 0)
+            {
+                Debug.Log("No trash to deposit");
+            }
         }
     }
 }
