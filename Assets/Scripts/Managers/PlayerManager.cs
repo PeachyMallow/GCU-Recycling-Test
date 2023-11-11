d3d29d7d5bbf461310755c9f1e7647c3a0acd70e
PeachyMallow <rebecca.abby.hughes@gmail.com>
2023-11-11 20:47:36 +0000

    Added UI Player Capacity & tidied up code

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    // the amount of rubbish the player can hold
+    [Header("Set the player's capacity")]
     [SerializeField]
     private int playerCapacity;
 
-    [SerializeField]
+    // what he player currently has in their inventory
     private int currentlyHolding;
 
+    [Header("Drag UI Manager GameObject here")]
     [SerializeField]
-    private bool inventoryFull;
+    private UIManager uiManager;
 
+    private void Start()
+    {
+        uiManager.UpdateCapacityUI(currentlyHolding, playerCapacity);
+    }
 
     // (accessed in RubbishInteraction.cs)
     /// <summary>
@@ -32,7 +39,7 @@ public class PlayerManager : MonoBehaviour
 
     // (accessed in RubbishInteraction.cs)
     /// <summary>
-    /// Updates the player's inventory
+    /// Updates the player's inventory upon player interaction with rubbish or the bin
     /// </summary>
     /// <param name="a"></param>
     public void UpdateInventory(int a, bool b)
@@ -46,5 +53,7 @@ public class PlayerManager : MonoBehaviour
         {
             currentlyHolding = a;
         }
+
+        uiManager.UpdateCapacityUI(currentlyHolding, playerCapacity);
     }
 }
