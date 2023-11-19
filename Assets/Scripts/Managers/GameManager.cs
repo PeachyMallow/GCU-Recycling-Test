59d43548ce3e369e70d4295446f34d0d2346bd3a
PeachyMallow <rebecca.abby.hughes@gmail.com>
2023-11-19 17:25:54 +0000

    Starting Item Spawning

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.UI.Image;
+using UnityEngine.UIElements;
 
 public class GameManager : MonoBehaviour
 {
+    #region timerVariables
+    [Header("\nTimer\n")]
     // timer set in inspector
     [Header("Enter time in seconds")]
     [SerializeField]
@@ -14,15 +18,46 @@ public class GameManager : MonoBehaviour
 
     // is the timer active
     private bool timerActive;
+    #endregion
+
+    [Header("\n\nLitter Spawn\n")]
+    // item spawn
+    [Header("Drag rubbish prefab here")]
+    [SerializeField]
+    private GameObject litter;
+
+    // time between items spawning
+    [Header("Time in seconds for litter spawning")]
+    [SerializeField]
+    private float litterSpawnTime;
+
+    // temp variables
+    // position of litter to be instantiated
+    [Header("Temp Variables")]
+    [Header("Input position for litter to spawn")]
+    [SerializeField]
+    private Vector3 litterPos;
+
+    // litter's parent
+    [Header("Drag Litter Parent GameObject here")]
+    [SerializeField]
+    private Transform litterParent;
+
+    // true if there is an item has been spawned
+    [SerializeField]
+    private bool readyToSpawn;
+
 
     private void Start()
     {
         totalTime = timer;
         timerActive = true;
+        readyToSpawn = true;
     }
 
     private void Update()
     {
+        #region timerUpdate
         if (timerActive)
         {
             if (timer > 0)
@@ -37,8 +72,16 @@ public class GameManager : MonoBehaviour
                 timerActive = false;            
             }
         }
+        #endregion
+
+        if (readyToSpawn)
+        {
+            readyToSpawn = false;
+            InstantiateItem();
+        }
     }
 
+    #region timerMethods
     private float Timer()
     {
         return timer -= Time.deltaTime;
@@ -65,5 +108,21 @@ public class GameManager : MonoBehaviour
     public bool IsTimeUp()
     {
         return timer <= 0;
+    }
+
+    #endregion
+
+    // item spawn
+    private void InstantiateItem()
+    {
+        Instantiate(litter, litterPos, Quaternion.identity, litterParent);
+        SpawnTimer();
+    }
+
+    // timer for item spawn
+    private IEnumerator SpawnTimer()
+    {
+        yield return new WaitForSeconds(litterSpawnTime);
+        readyToSpawn = true;
     }
 }
