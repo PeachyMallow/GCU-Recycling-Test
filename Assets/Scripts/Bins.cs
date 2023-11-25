1af4fd044d3b31a495df3e457bb7e5187e21d2ce
PeachyMallow <rebecca.abby.hughes@gmail.com>
2023-11-25 15:39:13 +0000

    Update Bins.cs

--- Assets/Scripts/Bins.cs
+++ Assets/Scripts/Bins.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Bins : MonoBehaviour
 {
-    
+    [Header("Amount this bin can hold")]
+    [SerializeField]
+    private int binCapacity;
+
+    [SerializeField]
+    private bool isBinFull;
 
     void Start()
     {
-        
+        isBinFull = false;
     }
 
 
